1e44ea7627785c79d1d47e1232cf74ad6aa1b785
남수정 <2020113388@dgu.ac.kr>
2025-11-12 17:51:19 +0900

    [init] 프로젝트 초기화

---
+++ happy/src/main/java/com/sujung/happy/HappyApplication.java
@@ -0,0 +1,13 @@
+package com.sujung.happy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyApplication.class, args);
+	}
+
+}
